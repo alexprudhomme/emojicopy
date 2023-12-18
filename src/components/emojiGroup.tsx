0f5b8ea2c776a9c9038d4d6e256c16f5b1b0cf68
Alex Prudhomme <prudhomme.alex42@gmail.com>
2023-12-17 21:21:17 -0500

    add search feature

--- src/components/emojiGroup.tsx
+++ src/components/emojiGroup.tsx
@@ -1,16 +1,47 @@
 import { component$ } from "@builder.io/qwik";
-import type { EmojiValues } from "~/routes";
+import type { EmojiGroup } from "~/routes";
 import Emoji from "./emoji";
 
-export default component$((props: { title: string; emojis: EmojiValues[] }) => {
-  return (
-    <div>
-      <h1 class="text-center">{props.title}</h1>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 ">
-        {props.emojis.map(({ character, slug }) => (
-          <Emoji key={slug} character={character} />
+export default component$(
+  (props: { emojis: { [key: string]: EmojiGroup } }) => {
+    return (
+      <div class="column">
+        {Object.entries(props.emojis).map(([group, emojisInGroup]) => (
+          <div key={group}>
+            <h1 class="text-center">{getTitle(group)}</h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 ">
+              {emojisInGroup.emojis.map(({ character, slug }) => (
+                <Emoji key={slug} character={character} />
+              ))}
+            </div>
+          </div>
         ))}
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
+
+function getTitle(group: string) {
+  switch (group) {
+    case "smileys-emotion":
+      return "Smileys & Emotion";
+    case "people-body":
+      return "People & Body";
+    case "animals-nature":
+      return "Animals & Nature";
+    case "food-drink":
+      return "Food & Drink";
+    case "travel-places":
+      return "Travel & Places";
+    case "activities":
+      return "Activities";
+    case "objects":
+      return "Objects";
+    case "symbols":
+      return "Symbols";
+    case "flags":
+      return "Flags";
+    default:
+      return "Other";
+  }
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
-import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import {
+  $,
+  component$,
+  useSignal,
+  useTask$,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import emojisData from "../../data/emojis.json";
 import EmojiGroup from "~/components/emojiGroup";
-export interface EmojiValues {
+interface EmojiValues {
   slug: string;
   character: string;
   unicodeName: string;
@@ -11,8 +17,7 @@ export interface EmojiValues {
   subGroup: string;
   variants?: Array<EmojiVariant>;
 }
-
-interface EmojiGroup {
+export interface EmojiGroup {
   group: string;
   title: string;
   emojis: EmojiValues[];
@@ -24,32 +29,64 @@ interface EmojiVariant {
 }
 
 export default component$(() => {
-  const emojis = useSignal<EmojiValues[]>();
+  const originalEmojis = useSignal<{ [key: string]: EmojiGroup }>();
+  const emojis = useSignal<{ [key: string]: EmojiGroup }>();
+
+  const query = useSignal<string>("");
 
   useTask$(() => {
-    emojis.value = emojisData;
+    const emojiGroupedMap: { [key: string]: EmojiGroup } = {};
+
+    emojisData.forEach((emoji) => {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (!emojiGroupedMap[emoji.group]) {
+        emojiGroupedMap[emoji.group] = {
+          group: emoji.group,
+          title: emoji.group,
+          emojis: [],
+        };
+      }
+
+      emojiGroupedMap[emoji.group].emojis.push(emoji);
+    });
+    originalEmojis.value = emojiGroupedMap;
+    emojis.value = emojiGroupedMap;
+  });
+
+  const searchedEmojis = $(() =>
+    searchForQuery(originalEmojis.value ?? {}, query.value)
+  );
+
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(({ track }) => {
+    track(() => query.value);
+
+    searchedEmojis().then((res) => {
+      emojis.value = res;
+    });
+  });
+
+  const setQuery = $((e: any) => {
+    query.value = e.target.value;
   });
 
   return (
     <>
       <div class="w-full bg-neon-carrot rounded-b-md flex">
         <div class="grow w-20 flex justify-center items-center">
-          <input></input>
+          <input onInput$={setQuery} placeholder="Search"></input>
         </div>
         <div class="grow-0 ">
           <h1 class="text-4xl mt-4 mb-4">Emojis 😃</h1>
         </div>
         <div class="grow w-20 flex justify-center items-center">
-          <div>01</div>
+          <div>01 {query}</div>
           <div>02</div>
         </div>
       </div>
-      {renderEmojiGroups(emojis.value!)}
-      {/* <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 ">
-          {emojis.value?.map(({ group, title, emojis }) => (
-            <EmojiGroup key={group} title={title} emojis={emojis} />
-          ))}
-        </div> */}
+      <div class="flex justify-center items-center">
+        <EmojiGroup emojis={emojis.value ?? {}}></EmojiGroup>
+      </div>
     </>
   );
 });
@@ -64,57 +101,19 @@ export const head: DocumentHead = {
   ],
 };
 
-function getTitle(group: string) {
-  switch (group) {
-    case "smileys-emotion":
-      return "Smileys & Emotion";
-    case "people-body":
-      return "People & Body";
-    case "animals-nature":
-      return "Animals & Nature";
-    case "food-drink":
-      return "Food & Drink";
-    case "travel-places":
-      return "Travel & Places";
-    case "activities":
-      return "Activities";
-    case "objects":
-      return "Objects";
-    case "symbols":
-      return "Symbols";
-    case "flags":
-      return "Flags";
-    default:
-      return "Other";
-  }
-}
-
-function renderEmojiGroups(emojis: EmojiValues[]) {
-  const emojieGroupedMap: { [key: string]: EmojiGroup } = {};
-
-  emojis.forEach((emoji) => {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (!emojieGroupedMap[emoji.group]) {
-      emojieGroupedMap[emoji.group] = {
-        group: emoji.group,
-        title: emoji.group,
-        emojis: [],
+function searchForQuery(emojis: { [key: string]: EmojiGroup }, query: string) {
+  const searchedEmojis: { [key: string]: EmojiGroup } = {};
+  const emojiValues = Object.values(emojis);
+  emojiValues.forEach((emojiGroup) => {
+    const emojis = emojiGroup.emojis.filter((emoji) => {
+      return emoji.unicodeName.includes(query);
+    });
+    if (emojis.length > 0) {
+      searchedEmojis[emojiGroup.group] = {
+        ...emojiGroup,
+        emojis,
       };
     }
-
-    emojieGroupedMap[emoji.group].emojis.push(emoji);
   });
-  return (
-    <div class="flex justify-center items-center">
-      <div class="column">
-        {Object.entries(emojieGroupedMap).map(([title, emojisInGroup]) => (
-          <EmojiGroup
-            key={title}
-            title={getTitle(title)}
-            emojis={emojisInGroup.emojis}
-          ></EmojiGroup>
-        ))}
-      </div>
-    </div>
-  );
+  return searchedEmojis;
 }
